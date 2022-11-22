213a32309af11b2f4931225882eabf225ec93c04
igorraknorwid <igorrakfed@gmail.com>
2022-11-22 14:29:59 +0100

    support woocommerce 2

--- functions.php
+++ functions.php
@@ -155,6 +155,14 @@ add_action( 'after_setup_theme', function(){
 		),
 ) );
 
+add_theme_support( 'wc-product-gallery-zoom' );
+add_theme_support( 'wc-product-gallery-lightbox');
+add_theme_support( 'wc-product-gallery-slider' );
+
+if ( ! isset( $content_width ) ) {
+	$content_width = 600;
+}
+
 } )
 ?>
 
